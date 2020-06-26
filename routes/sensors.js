432418fb11e05aa4bd53ed025daea4069ce32572
CalumBradbury24 <52780868+CalumBradbury24@users.noreply.github.com>
2020-06-26 13:07:51 +0100

    Update sensors.js

--- routes/sensors.js
+++ routes/sensors.js
@@ -1,9 +1,9 @@
-//Probably dont need the post requests since this is handled in python?
+
 const router = require('express').Router();
 let Sensor = require('../models/sensor.model');
 
 //Endpoint for incoming http GET requests on '/' URL path
-router.route('/').get((req, res) => {//where the request comes from?e.g front-end on heroku
+router.route('/').get((req, res) => {
   Sensor.find()
     .then(sensors => res.json(sensors))
     .catch(err => res.status(400).json('Error: ' + err));
